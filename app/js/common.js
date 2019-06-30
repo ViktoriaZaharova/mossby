07bc4a8bf11fcee4c3ae9dc538405777218da35a
ViktoriaZaharova <zaharova181114@gmail.com>
2019-06-30 19:12:29 +0300

    stages work one

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
-    console.log( "ready!" );
+    $('.main-slider').slick({
+        slidesToShow: 1,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        fade: true,
+        autoplay: true
+    });
+
+    $('.project-slider').slick({
+        dots: false,
+        arrows: true,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        slidesToShow: 3,
+        appendArrows: '.project-slider-nav'
+    });
+
+    $(".project-slider").on('afterChange', function(event, slick, currentSlide){
+        $(".cp").text(currentSlide<10?`${currentSlide+1}`:currentSlide+1);
+        $(".cp2").text(currentSlide<10?`0${currentSlide+1}`:currentSlide+1);
+    });
+
+    $('.letters-slider').slick({
+        slidesToShow: 5,
+        dots: true,
+        appendArrows: '.letters-slider-nav',
+        appendDots: '.letters-slider-nav',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>'
+    });
+
+
+    $('.products-slider').slick({
+        slidesToShow: 3,
+        dots: true,
+        appendArrows: '.products-slider-nav',
+        appendDots: '.products-slider-nav',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>'
+    });
 });
